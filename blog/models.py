ba9f3b1e3698623fd6a4adcb1d05cf483171f507
Pomsou <pablo.sbmol@gmail.com>
2019-01-15 16:36:54 -0300

    adding blog app for models/views

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,9 @@ class Blog(models.Model):
     post_date = models.DateTimeField()
     image = models.ImageField(upload_to='images/')
     body = models.TextField()
+
+    def summary(self):
+        return self.body[:60] + '...'
+
+    def post_date_field(self):
+        return self.post_date.strftime('%A %e, %Y')
